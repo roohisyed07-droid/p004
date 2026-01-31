9649c1f0b4f4747d2651c7650b13379f7ffac263
roohisyed07-droid <roohisyed07@gmail.com>
2026-01-31 10:36:30 +0530

    v1

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App1 from './App1.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <BrowserRouter basename='/p004/'>
         <Routes>
             <Route path='/' element={<App/>} />
             <Route path='/fc' element={<App1/>} />
